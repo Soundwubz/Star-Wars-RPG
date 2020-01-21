61c28aed4ed9c1f4bf9d73a8007ac7e92db9f8fc
Nate Foster <soundwubz@gmail.com>
2020-01-21 17:56:08 -0500

    Game functionality complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,34 +4,62 @@ $(document).ready(function() {
         {
             id: "0",
             name: "Darth Vader",
-            health: 100,
+            health: 115,
             attack: 10,
-            counterAttack: 20
+            counterAttack: 10,
+            resetStats: function() {
+                this.health = 115;
+                this.attack = 10;
+            },
+            increaseAttack: function() {
+                this.attack += 10;
+            }
         },
         {
             id: "1",
             name: "Kylo Ren",
-            health: 125,
+            health: 145,
             attack: 15,
-            counterAttack: 25
+            counterAttack: 15,
+            resetStats: function() {
+                this.health = 145;
+                this.attack = 15;
+            },
+            increaseAttack: function() {
+                this.attack += 15;
+            }
         },
         {
             id: "2",
             name: "Luke Skywalker",
-            health: 115,
+            health: 130,
             attack: 12,
-            counterAttack: 22
+            counterAttack: 12,
+            resetStats: function() {
+                this.health = 130;
+                this.attack = 12;
+            },
+            increaseAttack: function() {
+                this.attack += 12;
+            }
         },
         {
             id: "3",
             name: "Rey Skywalker",
-            health: 130,
-            attack: 15,
-            counterAttack: 25
+            health: 150,
+            attack: 20,
+            counterAttack: 15,
+            resetStats: function() {
+                this.health = 150;
+                this.attack = 15;
+            },
+            increaseAttack: function() {
+                this.attack += 20;
+            }
         }
     ];
 
-    function updateHealth(element, health) {
+    function updateHealth(element, health) { // updates character health on screen
 
         for(i = 0; i < element.length; i++){
             if(element[i].className === "health") {
@@ -41,8 +69,14 @@ $(document).ready(function() {
     }
 
     function initGame() {
+        usrChar = undefined;
+        defender = undefined;
         for(j = 0; j < characters.length; j++) {
-            var charDiv = $("#" + characters[j].id).children();
+            var char = $('#' + characters[j].id);
+            var charDiv = char.children();
+            $('#charSelect').append(char);
+            characters[j].resetStats();
+            char.css("display", "inline-block");
             updateHealth(charDiv, characters[j].health);
         }
     }
@@ -54,6 +88,14 @@ $(document).ready(function() {
     var defender;
 
     var inBattle = false;
+
+    function gameOver(character) {
+        if(inBattle) { // if currently in battle
+            $('#' + character.id).css("display", "none"); // removes defeated enemy from screen
+        } else { // if no longer in battle
+            initGame(); // reset game
+        }
+    }
     
     $(".character").on("click", function(){
         var id = parseInt($(this).attr("value"));
@@ -62,7 +104,7 @@ $(document).ready(function() {
             usrChar = characters[id];
             $('#userCharacter').append($(this)); // Moves character selected to #userCharacter;
 
-            for(i = 0; i < characters.length; i++) {
+            for(i = 0; i < characters.length; i++) { // Move other characters to #enemies div
                 if(i !== id) {
                     enemies.push(characters[i]);
                     $('#enemies').append($('#' + i));
@@ -74,26 +116,49 @@ $(document).ready(function() {
             defender = characters[id];
             inBattle = true;
         }
-        console.log($(this));
+        console.log('Character clicked: ' + $(this));
 
 
     });
 
     $("#attack-btn").on("click", function(){
         if(inBattle) {
-            defender.health -= usrChar.attack;
-            usrChar.health -= defender.counterAttack;
-            usrChar.attack += 3;
-
-            console.log(usrChar.name + " health: " + usrChar.health);
-            console.log(defender.name + " health: " + defender.health);
-
-            // setHealth(usrChar.health, defender.health);
-
             var charDiv = $("#" + usrChar.id).children();
             var defDiv = $("#" + defender.id).children();
+
+
+            defender.health -= usrChar.attack; 
+            usrChar.health -= defender.counterAttack;
             updateHealth(charDiv, usrChar.health);
             updateHealth(defDiv, defender.health);
+            
+            console.log(usrChar.name + " health: " + usrChar.health);
+            console.log(defender.name + " health: " + defender.health);
+            
+            if(defender.health < 1) {
+                gameOver(defender);
+                if(usrChar.health < 1) { // both defender and user fell below 1 hp
+                    inBattle = false;
+                    alert(usrChar.name + " and " + defender.name + " have both been defeated");
+                    gameOver(usrChar);
+                } else { // only defender fell below 1 hp
+                    alert(defender.name + " has been defeated!");
+                }
+                return false;
+            }
+            else if( usrChar.health < 1) { // User has been defeted
+                inBattle = false;
+                alert(usrChar.name + " has been defeated!");
+                gameOver(usrChar);
+                return false;
+            } 
+            else { // Neither characters defeated; Increase usrChar attack
+                usrChar.increaseAttack();
+            }
+
+
+            console.log('In battle: ' + inBattle);
+
         }
     });
 });
